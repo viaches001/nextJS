d56bfd8f57117b8137910ac4bccff9e291e39b36
viacheslav <viacheslavbereslavets@gmail.com>
2023-11-09 21:35:03 +0300

    final commit

---
+++ src/services/parse/functions/user.ts
@@ -0,0 +1,25 @@
+import { User, serverURL } from 'parse'
+import { IProfile } from "@/props/IProfile"
+import models from '../../models'
+
+export const setUserInterests = async (interests:Parse.Object<Parse.Attributes>[]) => {
+    try {
+        if(!interests) return
+        let currentUser = User.current();
+        currentUser?.relation(models.user.params.interests).add(interests);
+        currentUser?.save();
+    } catch (error:any) {
+        throw new Error(error.message);
+    }
+}
+    
+export const getCurrentUserAsJson = (): IProfile | null => {
+    if (serverURL) {
+        let user = User.current();
+    
+        let userAsJson = user?.toJSON();
+    
+        return userAsJson ? {email:userAsJson?.email, objectId:userAsJson?.objectId} : null;
+    }
+    return null;
+}
